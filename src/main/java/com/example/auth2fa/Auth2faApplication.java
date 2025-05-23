a0dc5589343ce6cd0f438874959c9e8b197a7a58
Cedric MACE <cedric.mace@open-groupe.com>
2025-05-23 11:18:27 +0200

    Initial commit with Spring Boot project

---
+++ src/main/java/com/example/auth2fa/Auth2faApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth2fa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth2faApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth2faApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/auth2fa/Auth2faApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth2fa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth2faApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
